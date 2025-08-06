e2163187d7d93161230af3b12b1c1b60934df9c7
Wepa <wepa86802@example.com>
2025-08-06 16:19:43 +0500

    Update authentication

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
@@ -24,7 +26,21 @@ class LoginRequest extends FormRequest
     public function authenticate()
     {
         $credentials = $this->only('phone', 'password');
+        $phone = $this->input('phone');
+        $password = $this->input('password');
+        $user = User::where('phone', $phone)->first();
 
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'phone' => __('Not Registered number'),
+            ]);
+        }
+
+        if (!Hash::check($password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => __('Incorrect password'),
+            ]);
+        }
         if (!Auth::attempt($credentials)) {
             throw ValidationException::withMessages([
                 'phone' => __('Not Registered number'),
